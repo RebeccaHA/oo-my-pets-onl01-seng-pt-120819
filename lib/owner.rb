32e8567e34d7c3644b9308ea517171bf206a098f
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-14 19:26:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,12 +43,12 @@ class Owner
   
   def buy_cat(name)
     cat = Cat.new(name, self)
-    @pets[:cats].store(cat)
+    cat
   end
   
    def buy_dog(name)
     dog = Dog.new(name, self)
-    @pets[:dogs].store(dog)
+    dog
   end
   
   def walk_dogs
