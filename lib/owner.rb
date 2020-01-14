7e5f92f5cd99f270a15f852ae10f38b85d1b4fca
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-14 19:45:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,7 @@ class Owner
 end
   
   def list_pets
-    "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
   end
 
 end
